aae62d0f92e97cdc651395cce6c80f04fdcb61fd
lqjzxc123 <493663519@qq.com>
2016-12-07 10:46:24 +0800

    Update Main.java

--- src/com/example/main/Main.java
+++ src/com/example/main/Main.java
@@ -18,7 +18,9 @@ public class Main extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+        
+        bfbxcxcbbxcxcb
     }
    
 }
-bfbxcxcbbxcxcb
+
